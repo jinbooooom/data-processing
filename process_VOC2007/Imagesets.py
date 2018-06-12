62f7de6742a52c28c2e148c60b74f96cd2ab7c8e
Jinbo <jinboom@foxmail.com>
2018-06-12 16:05:28 +0800

    none

--- process_VOC2007/Imagesets.py
+++ process_VOC2007/Imagesets.py
@@ -9,37 +9,36 @@ trainval占总数据集的50%，test占总数据集的50%；train占trainval的5
 
 import os,sys
 import random
-from jinbo_os_lib import file_name,postfix
-xmlfilepath = './test2/xml/'
-txtsavepath = './test2/ImageSets/Main/'
-trainval_percent = 0.5 #trainval占整个数据集的百分比，剩下部分就是test所占百分比
-train_percent = 0.5 #train占trainval的百分比，剩下部分就是val所占百分比
 
-names = []
-file_name(names,xmlfilepath)
-total = len(names[0])
+xmlfilepath = './Annotations'
+txtsavepath = './ImageSets/Main'
+trainval_percent = 0.9 #trainval占整个数据集的百分比，剩下部分就是test所占百分比
+train_percent = 0.8 #train占trainval的百分比，剩下部分就是val所占百分比
+
+names = [x for x in os.listdir(xmlfilepath)]
+total = len(names)
 test_num = int(total * (1 - trainval_percent))
 train_num = int(total * trainval_percent * train_percent)
-print(names)
-print(total)
-random.shuffle(names[0])
+#print(names)
+print('total:%d' % total)
+random.shuffle(names)
 #names = random.sample(names[0],total) #这行也可以：随机返回names[0]中total个元素
-print(names)
-with open(txtsavepath + 'test.txt','w') as ftest:
-	test = names[0][:test_num]
+#print(names)
+with open(os.path.join(txtsavepath,'test.txt'),'w') as ftest:
+	test = names[:test_num]
 	#print(test)
 	for x in test:
 		ftest.write(x[:6] + '\n')
-with open(txtsavepath + 'train.txt','w') as ftrain:
-	train = names[0][test_num:test_num + train_num]
+with open(os.path.join(txtsavepath,'train.txt'),'w') as ftrain:
+	train = names[test_num:test_num + train_num]
 	for x in train:
 		ftrain.write(x[:6] + '\n')
-with open(txtsavepath + 'val.txt','w') as fval:
-	val = names[0][test_num + train_num:]
+with open(os.path.join(txtsavepath,'val.txt'),'w') as fval:
+	val = names[test_num + train_num:]
 	for x in val:
 		fval.write(x[:6] + '\n')
-with open(txtsavepath + 'trainval.txt','w') as ftrainval:
-	trainval = names[0][test_num:]
+with open(os.path.join(txtsavepath,'trainval.txt'),'w') as ftrainval:
+	trainval = names[test_num:]
 	for x in trainval:
 		ftrainval.write(x[:6] + '\n')	
 
